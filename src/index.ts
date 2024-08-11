125168cc973d9c65992dbbe922c5bc7ba9fc203c
to1dev <admin@to1.dev>
2024-08-11 02:22:24 +0200

    add more funcs and database operations

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,20 @@ const PUBLIC_ELECTRUMX_ENDPOINT1 = 'blockchain.atomicals.find_realms';
 const PUBLIC_ELECTRUMX_ENDPOINT2 = 'blockchain.atomicals.get_state';
 const PUBLIC_ELECTRUMX_ENDPOINT3 = 'blockchain.atomicals.list';
 
-interface Realm {
+interface RealmResult {
     atomical_id: string;
     realm: string;
     realm_hex: string;
     status: string;
     tx_num: number;
+}
+
+interface RealmData {
+    id: string;
+    number: number;
+    mintAddress: string;
+    address: string;
+    pid: string;
 }
 
 const mainnet = {
@@ -35,48 +43,85 @@ function scriptAddress(hexScript: string): string | null {
     return parsedAddress;
 }
 
-async function processRealms(results: Realm[]) {
+async function saveToD1(env: Env, realm: string, data: RealmData): Promise<boolean> {
+    async function _exists(realm: string): Promise<boolean> {
+        const sql = `SELECT RealmName FROM _realms WHERE RealmName = ?1 LIMIT 1`;
+        const _realm = await env.MY_DB.prepare(sql).bind(realm).first();
+        return _realm !== null;
+    }
+
+    async function _save(): Promise<boolean> {
+        const { success } = await env.MY_DB.prepare(
+            `insert into _realms (RealmName, RealmId, RealmNumber, RealmMinter, RealmOwner, ProfileId) values (?1, ?2, ?3, ?4, ?5, ?6)`
+        )
+            .bind(realm, data?.id, data?.number, data?.mintAddress, data?.address, data?.pid)
+            .run();
+        return success;
+    }
+
+    async function _update(): Promise<boolean> {
+        const { success } = await env.MY_DB.prepare(
+            `update _realms set
+                RealmOwner = ?1,
+                ProfileId = ?2
+             where RealmName = ?3`
+        )
+            .bind(data?.address, data?.pid, realm)
+            .run();
+        return success;
+    }
+
+    const exists = await _exists(realm);
+    if (!exists) {
+        return await _save();
+    } else {
+        return await _update();
+    }
+
+    return false;
+}
+
+async function getRealm(id: string): Promise<any | null> {
+    const endpoint = PUBLIC_ELECTRUMX_ENDPOINT2;
+    const path: string = `${endpoint}?params=["${id}"]`;
+
+    try {
+        const res = await fetchApiServer(path);
+        if (!res.ok) {
+            throw new Error(`Error fetching data: ${res.statusText}`);
+        }
+
+        const data: any = await res.json();
+        if (!data) {
+            return null;
+        }
+
+        if (!data?.success) {
+            console.error(`Error getting right json result: ${res.statusText}`);
+            return null;
+        }
+
+        const number = data.response?.result?.atomical_number;
+        let mintAddress = scriptAddress(data.response?.result?.mint_info?.reveal_location_script);
+        let address = scriptAddress(data.response?.result?.location_info[0]?.script);
+        const pid = data.response?.result?.state?.latest?.d;
+
+        return { id, number, mintAddress, address, pid };
+    } catch (e) {
+        console.error('Failed to fetch realm:', e);
+        return null;
+    }
+}
+
+async function processRealms(env: Env, results: RealmResult[]) {
     if (results.length > 0) {
         const endpoint = PUBLIC_ELECTRUMX_ENDPOINT2;
 
         for (const result of results) {
             const realm = result?.realm;
             const id = result?.atomical_id;
-            const path: string = `${endpoint}?params=["${id}"]`;
-
-            try {
-                const res = await fetchApiServer(path);
-                if (!res.ok) {
-                    throw new Error(`Error fetching data: ${res.statusText}`);
-                }
-
-                const data: any = await res.json();
-                if (!data) {
-                    return;
-                }
-
-                if (!data?.success) {
-                    console.error(`Error getting right json result: ${res.statusText}`);
-                    return;
-                }
-
-                const number = data.response?.result?.atomical_number;
-                let mintAddress = scriptAddress(data.response?.result?.mint_info?.reveal_location_script);
-                let address = scriptAddress(data.response?.result?.location_info[0]?.script);
-                const pid = data.response?.result?.state?.latest?.d;
-
-                const _results = {
-                    realm,
-                    id,
-                    number,
-                    mintAddress,
-                    address,
-                    pid,
-                };
-            } catch (e) {
-                console.error('Failed to fetch realm profile id:', e);
-                return;
-            }
+            const data = await getRealm(id);
+            await saveToD1(env, realm, data);
         }
     }
 }
@@ -115,7 +160,7 @@ async function getRealms(env: Env, ctx: ExecutionContext): Promise<void> {
                 return;
             }
 
-            await processRealms(results);
+            await processRealms(env, results);
 
             totalFetched += results.length;
             if (results.length < pageSize) {
@@ -164,11 +209,11 @@ async function getLatestRealms(env: Env, ctx: ExecutionContext): Promise<void> {
             for (const result of results) {
                 const realm = result?.$full_realm_name;
                 const id = result?.atomical_id;
-                const number = result?.atomical_number;
                 const type = result?.type;
                 const subtype = result?.subtype;
                 if (type === 'NFT' && ['realm', 'subrealm'].includes(subtype)) {
-                    console.log(realm, id, number, type, subtype);
+                    const data = await getRealm(id);
+                    await saveToD1(env, realm, data);
                 }
             }
         }
@@ -190,12 +235,18 @@ export default {
     async scheduled(event, env, ctx): Promise<void> {
         switch (event.cron) {
             case '*/10 * * * *':
-                await getLatestRealms(env, ctx);
+                //await getLatestRealms(env, ctx);
                 break;
 
-            case '*0 0 * * 1':
-                //await getRealms(env, ctx);
-                console.log('every week');
+            case '* * * * *':
+                const cacheKey = `counter:fetch-realms`;
+                const cachedData = await env.api.get(cacheKey, { type: 'json' });
+                if (cachedData) {
+                } else {
+                    console.log('every week');
+                    await getRealms(env, ctx);
+                    ctx.waitUntil(env.api.put(cacheKey, JSON.stringify({ counter: 1 })));
+                }
                 break;
 
             default:
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,4 +1,5 @@
 interface Env {
+    api: KVNamespace;
     SERVICE_X_DATA: { DEBUG: true };
     MY_DB: D1Database;
 }
